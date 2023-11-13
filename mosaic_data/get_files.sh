c4921438e2628a048410fb4c3a61bd4733d25905
Lucas Sterzinger <lucassterzinger@gmail.com>
2023-11-12 17:27:48 -0800

    Delete mosaic_data/get_files.sh

--- mosaic_data/get_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -L -o aerosol-data.zip https://doi.pangaea.de/10.1594/PANGAEA.943907?format=zip
-curl -L -o met-data.zip https://doi.pangaea.de/10.1594/PANGAEA.952341?format=zip
-
-unzip aerosol-data.zip -d aerosol-data
-unzip met-data.zip -d met-data
